b42b12d8a1fed9f127340f3c102c0f0fb55a7b7e
shariyer-shawon <shariyer.shawon1@gmail.com>
2026-08-08 08:20:36 +0600

    Update Sum.java

--- shawon48/Sum.java
+++ shawon48/Sum.java
@@ -4,13 +4,15 @@ public class Sum {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        double sum = input.nextDouble();
+        System.out.print("Enter your last number of the series: ");
         int n = input.nextInt();
-        int i = input.nextInt();
+        double sum = 0.0;
+        int i ;
 
-        for(i; i>=n; i++){
-            sum = sum + i
+        for(i=1; i<=n; i++){
+            sum = sum + i/ (Math.pow(2, i));
         }
+        System.out.println("Sum of the series: " + sum);
         input.close();
     } 
 }
